104306edbf554beeeb92a9b8568e538ab1bc4efa
Hideonkat <danghung0711dh@gmail.com>
2023-12-08 18:22:07 +0700

    them Hienthiten

---
+++ hienthiten/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime;
+
+namespace diencaitenvao;
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Dien cai ten vao: ");
+        string ten = Console.ReadLine();
+        Console.WriteLine("Xin chao mung den bo nao cua toi:" + ten);
+}
+}
