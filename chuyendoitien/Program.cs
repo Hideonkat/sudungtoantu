98ce097c015eb67da2bcaf407147f9224a634a53
Hideonkat <danghung0711dh@gmail.com>
2023-12-08 18:49:55 +0700

    them chuyendoitien

---
+++ chuyendoitien/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace chuyendoitiente;
+
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        uint Do, Vn;
+        Console.WriteLine("USD:");
+        Do = uint.Parse(Console.ReadLine());
+        Console.WriteLine("Vnd:" + Do*23000);
+
+}
+}
