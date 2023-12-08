58c6c78be74eda0e5c49b2656461342bdb1a94f0
Hideonkat <danghung0711dh@gmail.com>
2023-12-08 18:10:39 +0700

    them hcn

---
+++ sudungtoantu2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace toantucoban;
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        float cao, rong;
+        Console.WriteLine("S HCN");
+        Console.WriteLine("Nhap chieu cao vao nhanh: ");
+        cao = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu rong vao mau: ");
+        rong = float.Parse(Console.ReadLine());
+        Console.WriteLine("Dien tich hcn la: " + cao*rong);
+
+
+
+        }
+}
